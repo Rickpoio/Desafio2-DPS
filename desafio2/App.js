cffc571b88f283dae14fc3ad972a5b24072d6eb7
Jhm2003 <92344283+Jhm2003@users.noreply.github.com>
2025-09-06 09:53:13 -0600

    cambios 1

--- desafio2/App.js
+++ desafio2/App.js
@@ -1,26 +1,19 @@
-import React, { useState, useEffect } from "react";
-import {
-  Text,
-  StyleSheet,
-  View,
-  FlatList,
-  TouchableHighlight,
-  TouchableWithoutFeedback,
-  Keyboard,
-  Platform,
-} from "react-native";
-import Cita from "./componentes/Cita";
-import Formulario from "./componentes/Formulario";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import Colors from "./src/utils/colors";
+import React, { useState, useEffect } from 'react';
+import { Text,  StyleSheet,  View,  FlatList,  TouchableHighlight,  TouchableWithoutFeedback,  Keyboard,  Platform,} from 'react-native';
+import Cita from './src/components/Cita';
+import Formulario from './src/components/Formulario';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Colors from './src/utils/colors';
+
 const App = () => {
   // definir el state de citas
   const [citas, setCitas] = useState([]);
   const [mostrarform, guardarMostrarForm] = useState(false);
+  
   useEffect(() => {
     const obtenerCitasStorage = async () => {
       try {
-        const citasStorage = await AsyncStorage.getItem("citas");
+        const citasStorage = await AsyncStorage.getItem('citas');
         if (citasStorage) {
           setCitas(JSON.parse(citasStorage));
         }
@@ -30,6 +23,7 @@ const App = () => {
     };
     obtenerCitasStorage();
   }, []);
+
   // Elimina los pacientes del state
   const eliminarPaciente = (id) => {
     const citasFiltradas = citas.filter((cita) => cita.id !== id);
@@ -47,23 +41,24 @@ const App = () => {
   // Almacenar las citas en storage
   const guardarCitasStorage = async (citasJSON) => {
     try {
-      await AsyncStorage.setItem("citas", citasJSON);
+      await AsyncStorage.setItem('citas', citasJSON);
     } catch (error) {
       console.log(error);
     }
   };
+
   return (
     <TouchableWithoutFeedback onPress={() => cerrarTeclado()}>
       <View style={styles.contenedor}>
         <Text style={styles.titulo}>Administrador de Citas</Text>
+
         <View>
           <TouchableHighlight
             onPress={() => mostrarFormulario()}
-            style={styles.btnMostrarForm}
-          >
+            style={styles.btnMostrarForm}>
             <Text style={styles.textoMostrarForm}>
-              {" "}
-              {mostrarform ? "Cancelar Crear Cita" : "Crear Nueva Cita"}{" "}
+              {' '}
+              {mostrarform ? 'Cancelar Crear Cita' : 'Crear Nueva Cita'}{' '}
             </Text>
           </TouchableHighlight>
         </View>
@@ -81,10 +76,10 @@ const App = () => {
           ) : (
             <>
               <Text style={styles.titulo}>
-                {" "}
+                {' '}
                 {citas.length > 0
-                  ? "Administra tus citas"
-                  : "No hay citas, agrega una"}{" "}
+                  ? 'Administra tus citas'
+                  : 'No hay citas, agrega una'}{' '}
               </Text>
               <FlatList
                 style={styles.listado}
@@ -101,22 +96,23 @@ const App = () => {
     </TouchableWithoutFeedback>
   );
 };
+
 const styles = StyleSheet.create({
   contenedor: {
     backgroundColor: Colors.PRIMARY_COLOR,
     flex: 1,
   },
   titulo: {
-    color: "#FFF",
-    marginTop: Platform.OS === "ios" ? 40 : 20,
+    color: '#FFF',
+    marginTop: Platform.OS === 'ios' ? 40 : 20,
     marginBottom: 20,
     fontSize: 24,
-    fontWeight: "bold",
-    textAlign: "center",
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
   contenido: {
     flex: 1,
-    marginHorizontal: "2.5%",
+    marginHorizontal: '2.5%',
   },
   listado: {
     flex: 1,
@@ -127,9 +123,9 @@ const styles = StyleSheet.create({
     marginVertical: 10,
   },
   textoMostrarForm: {
-    color: "#FFF",
-    fontWeight: "bold",
-    textAlign: "center",
+    color: '#FFF',
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
 export default App;
